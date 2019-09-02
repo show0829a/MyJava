22bcc07fa9430bae28d4536c0460946a10e7931a
Seung Hwan Kim <hwan0829a@gmail.com>
2019-09-02 17:11:46 +0900

    09/02

---
+++ jsp04/src/com/study/servlet/HelloWorld.java
@@ -0,0 +1,35 @@
+package com.study.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hw")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+    public HelloWorld() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello World</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		
+		doGet(request, response);
+	}
+
+}
---
+++ programersCodingTest/src/level_1/Quiz_01.java
@@ -0,0 +1,18 @@
+package level_1;
+
+
+class Solution {
+	 
+  public double solution(int[] arr) {
+	  int sum = 0;
+	  for(int i = 0 ; i < arr.length; i++) {
+		  sum += arr[i];
+	  }		  
+      double answer = 0;
+      answer = sum/arr.length;
+      String[] ne; 
+      ne.
+      return answer;
+  }
+}
+
